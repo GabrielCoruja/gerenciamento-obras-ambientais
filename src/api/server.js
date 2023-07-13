c54bbd3dc7cc7cfe05bc7c52a9c93ac2879beafb
Gabriel Dalseco <gabriel_santos96@outlook.com>
2023-07-12 21:39:34 -0300

    teste

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+const database = process.env.MYSQL_DATABASE || 'database_development';
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${database} ${port}`);
 });
